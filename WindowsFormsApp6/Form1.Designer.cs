cbc9c1d330ca0bdd2284b4651b657023f8a02476
Deo Narayan <deonara447@ed.amdsb.ca>
2020-05-11 15:57:21 -0400

    Add project files.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(42, 228);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(719, 144);
+            this.output.TabIndex = 1;
+            this.output.Text = "output";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(264, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 31);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "input int";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 54);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "fib";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(476, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 54);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "sum";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int input = Convert.ToInt16(textBox1.Text);
+            int solvedSum = Sum(input);
+            output.Text = "the sum is " + Convert.ToString(solvedSum);
+        }
+
+
+        public int Sum(int n)
+
+        {
+
+            if (n == 0 || n == 1)
+
+            {
+
+                return n;
+
+            }
+
+            else
+
+            {
+
+                return (n + Sum(n - 1));
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stopwatch myWatch = new Stopwatch();
+
+            int input = Convert.ToInt16(textBox1.Text);
+
+            myWatch.Reset();
+            myWatch.Start();
+            int recursive = recursiveFibonnaci(input);
+            myWatch.Stop();
+            TimeSpan elapsed2 = myWatch.Elapsed;
+
+            myWatch.Reset();
+            myWatch.Start();
+            int nonRecursive = iterativeFibonnaci(input);
+            myWatch.Stop();
+            TimeSpan elapsed1 = myWatch.Elapsed;
+
+            
+
+
+            output.Text = "non recursive value is " + Convert.ToString(nonRecursive) + "\n"
+                + "this method took " + Convert.ToString(elapsed1.TotalMilliseconds) + " ms" + "\n"
+                + "recursive value is " + Convert.ToString(recursive) + "\n" +
+                "this method took " + Convert.ToString(elapsed2.TotalMilliseconds) + " ms" ;
+        }
+
+
+        int iterativeFibonnaci(int nSize)
+        {
+            int nMinus2 = 0;
+            int nMinus1 = 1;
+            int n = 0;
+            int counter = nSize;
+
+            for (int x = 2; x <= counter; x++)
+            {
+                n = nMinus2 + nMinus1;
+                nMinus2 = nMinus1;
+                nMinus1 = n;
+            }
+
+            if (nSize == 0)
+                return 0;
+            else if (nSize == 1)
+                return 1;
+            else
+                return n;
+        }
+
+        int recursiveFibonnaci(int nSize)
+        {
+            switch (nSize)
+            {
+                case 0:
+                    return 0;
+                    
+                case 1:
+                    return 1;
+                    
+                default:
+                    return recursiveFibonnaci((nSize - 2)) + recursiveFibonnaci((nSize - 1));
+            }
+        }
+    }
+}
